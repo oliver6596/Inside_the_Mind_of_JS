e46abd963244a3cba67ebc47bbaf80a8a84079e0
oliver6596 <Q5115368@live.tees.ac.uk>
2017-12-07 22:51:38 +0000

    Post Processing and Idle added to start screen

--- Assets/Scripts/Player/LookAtMouse.cs
+++ Assets/Scripts/Player/LookAtMouse.cs
@@ -2,15 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LookAtMouse : MonoBehaviour {
+public class LookAtMouse : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public int moveSpeed = 80;
+    public Rigidbody playerRigidbody;   // Reference to the player's rigidbody.
+    int floorMask;
+    Vector3 newPos;
+    Vector2 inputVector;
+    public float movePower = 10f;
+    public float slowPower = .4f;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Runs racast function. 
+        Turning();
+    }
+
+    private void Awake()
+    {
+        // Create a layer mask for the floor layer.
+        floorMask = LayerMask.GetMask("HitBox");
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+
+    void Turning()
+    {
+        // Create a ray from the mouse cursor on screen in the direction of the camera.
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // Create a RaycastHit variable to store information about what was hit by the ray.
+        RaycastHit floorHit;
+
+        // Perform the raycast and if it hits something on the floor layer...
+        if (Physics.Raycast(camRay, out floorHit, Mathf.Infinity, floorMask))
+        {
+            // Create a vector from the player to the point on the floor the raycast from the mouse hit.
+            Vector3 playerToMouse = floorHit.point - transform.position;
+
+            // Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+
+            // Set the player's rotation to this new rotation.
+            playerRigidbody.MoveRotation(newRotation);
+        }
+    }
+
 }
